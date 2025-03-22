afc6146c136e70c83b55666d348bfd8852833ea4
ADT <168193058+aryandevtyagi10@users.noreply.github.com>
2025-03-22 21:22:56 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+# def count(s):
+#     for str in s.split():
+#         s = "&".join(str)
+#     return s
+
+# print(count("Python is fun to learn."))
+list = [1,23,4,5];
+for i in list:
+  print(i)
+  
